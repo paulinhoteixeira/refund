e027f0be2bd57956c9b4e29acaa173078e965dfa
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-10-16 21:35:58 -0300

    funtion to show expenses in the list

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 const form = document.querySelector("form")
 
+const expenseList = document.querySelector("ul")
+
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
 
@@ -33,5 +35,45 @@ form.onsubmit = (event) => {
     create_at: new Date()
 
   }
+  expenseAdd(newExpense)
+} 
 
+function expenseAdd(newExpense) {
+  try {
+    const expenseItem = document.createElement("li")
+    expenseItem.classList.add("expense")
+    
+    const expenseIcon = document.createElement("img")
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+    expenseIcon.setAttribute("alt", newExpense.category_name)
+
+    const expenseInfo = document.createElement("div")
+    expenseInfo.classList.add("expense-info")
+
+    const expenseName = document.createElement("strong")
+    expenseName.textContent = newExpense.expense
+
+    const expenseCategory = document.createElement("span")
+    expenseCategory.textContent = newExpense.category_name
+
+    expenseInfo.append(expenseName, expenseCategory)
+
+    const expenseAmount = document.createElement("span")
+    expenseAmount.classList.add("expense-amount")
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount
+      .toUpperCase()
+      .replace("R$", "")}`
+
+    const removeIcon = document.createElement("img")
+    removeIcon.classList.add("remove-icon")
+    removeIcon.setAttribute("src", "img/remove.svg")
+    removeIcon.setAttribute("alt", "Remover")
+
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
+    expenseList.append(expenseItem)
+
+  } catch (error) {
+    alert("Não foi possível atualizar a lista de despesas.")
+    console.log(error)
+  }
 }
