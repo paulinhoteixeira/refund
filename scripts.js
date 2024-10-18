6d2c8cd52f6763ecc830556f543c0ac644ce4772
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-10-18 00:30:29 -0300

    funtion to update and show expenses quantity and expenses total

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ const category = document.getElementById("category")
 const form = document.querySelector("form")
 
 const expenseList = document.querySelector("ul")
+const expensesQuantity = document.querySelector("aside header p span")
+const expensesTotal = document.querySelector("aside header h2")
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
@@ -72,8 +74,40 @@ function expenseAdd(newExpense) {
     expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
     expenseList.append(expenseItem)
 
+    updateTotals()
+
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.")
     console.log(error)
+  }
+}
+
+function updateTotals(){
+  try {
+    const items = expenseList.children
+
+    expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+    let total = 0
+
+    for(let item = 0; item < items.length; item++){
+      const itemAmount = items[item].querySelector(".expense-amount")
+
+      let value = itemAmount.textContent.replace(/[^\d]/g, "").replace(",",".")
+      value = parseFloat(value)
+
+      if(isNaN(value)){
+        return alert("Não foi possível calcular o total. O valor não parece ser um número.")
+      }
+
+      total = total + Number(value)
+    }
+
+    expensesTotal.textContent = total
+    
+  } catch (error) {
+    console.log(error)
+    alert("Não foi possível atualizar os totais.")
+    
   }
 }
