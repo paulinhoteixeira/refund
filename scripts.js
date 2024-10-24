8c5a0bdd6161bda2fae7ea9511ad618f7794e8cb
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-10-23 22:23:25 -0300

    function to clear the form

--- scripts.js
+++ scripts.js
@@ -74,6 +74,7 @@ function expenseAdd(newExpense) {
     expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
     expenseList.append(expenseItem)
 
+    formClear()
     updateTotals()
 
   } catch (error) {
@@ -128,3 +129,11 @@ expenseList.addEventListener("click", function(event) {
 
   updateTotals()
 })
+
+function formClear() {
+  expense.value = ""
+  category.value = "" 
+  amount.value = ""
+
+  expense.focus()
+}
